a97592076252e60ecc2399e6387fe518740f1c4c
Vinicius Cunha <vicunha@outlook.com>
2024-11-01 22:13:01 +0000

    Lab implementation

--- Program.cs
+++ Program.cs
@@ -1,3 +1,35 @@
 ﻿
 
+using Lab_e575_Windows_BlazorComponents_ConsoleApp;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+IServiceCollection serviceCollection = new ServiceCollection();
+serviceCollection.AddLogging();
+
+IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+ILoggerFactory loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+
+await using var htmlRender = new HtmlRenderer(serviceProvider, loggerFactory);
+
+var html = await htmlRender.Dispatcher.InvokeAsync
+    (
+        async () =>
+        {
+            var dictionary = new Dictionary<string, Object?>
+            {
+                { "Message", "This is a secrect message" }
+            };
+
+            var parameters = ParameterView.FromDictionary(dictionary);
+            var output = await htmlRender.RenderComponentAsync<BasicMessage>(parameters);
+
+            return output.ToHtmlString();
+        }
+    );
+
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine(html);
